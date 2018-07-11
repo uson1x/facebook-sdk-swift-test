bfb6d8250128fcf9ffd9fec1959be6b594a79d92
Ivan Parfenchuk <uson1x@gmail.com>
2018-07-11 16:45:06 +0300

    Initial commit

---
+++ FacebookSDKTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FacebookSDKTest
+//
+//  Created by Ivan Parfenchuk on 11/07/2018.
+//  Copyright © 2018 uspec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
